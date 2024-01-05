b51070a51768875864395f1783c88df178fd875e
jerinzech <jerinzjames@gmail.com>
2024-01-05 11:30:03 +0530

    react day 2

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./styles.css";
+
+
+
+export default function App() {
+  
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] =
+
+  return (
+    <>
+      <form className="new-item-form" action="">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" value={newItem} onChange={e=> setNewItem(e.target.value)}/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+}
