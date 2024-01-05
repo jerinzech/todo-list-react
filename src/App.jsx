d739bf153683986f1ae6a9ec78e2b9383df3e32d
jerinzech <jerinzjames@gmail.com>
2024-01-05 14:08:09 +0530

    Refactored components and added peristence using local storage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
-
-
+import { NewTodoForm } from "./NewTodoForm";
+import TodoList from "./TodoList";
 
 export default function App() {
-  
-  const [newItem, setNewItem] = useState("");
-  const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault()
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  });
 
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  },[todos])
+
+  function addTodo(title) {
     setTodos(currentTodos => {
       return [
         ...currentTodos,
-        {id:crypto.randomUUID(), title:newItem, completed:false},
+        {id:crypto.randomUUID(), title, completed:false},
       ]
     })
-    setNewItem("")
 
   }
 
@@ -40,34 +44,16 @@ export default function App() {
 
   }
  
-  console.log(todos)
+  // console.log(todos)
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form" action="">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" value={newItem} onChange={e=> setNewItem(e.target.value)}/>
-        </div>
-        <button className="btn" >Add</button>
-      </form>
-      <h1 className="header">Todo list</h1>
-      <ul className="list">
-        {todos.length === 0 && "No Todos"}
-        {todos.map(todo => {
-          return (  
-          <li key={todo.id}>
-            <label>
-              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-              {todo.title}
-            </label>
-            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger" >Delete</button>
-          </li>);
-         
-        })}
-       
-      </ul>
-      
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo list</h1> 
+      <TodoList 
+        todos={todos} 
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo} />
     </>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+
+export default function TodoList({ todos, toggleTodo, deleteTodo}) {
+  return (
+    <div>
+        <ul className="list">
+            {todos.length === 0 && "No Todos"}
+            {todos.map(todo => {
+                return (
+                <TodoItem 
+                  {...todo} 
+                  key={todo.id}
+                  toggleTodo={toggleTodo}
+                  deleteTodo={deleteTodo}
+                />
+                ); 
+                
+            })}
+        
+        </ul>
+    </div>
+  )
+}
